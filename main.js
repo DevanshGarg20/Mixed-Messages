00134bf8610241d546717a616dd7c9a24cb7b8a2
Devansh Garg <dg.devansh.dg@gmail.com>
2020-12-04 03:58:38 +0530

    made the object parts having parts of phrases

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function randomNum(size){
   return Math.floor(Math.random() * size)
 }
 
+const parts = {
+  pushYourself: ["Do it", "Do it now", "because no one else is going to do it for you","you will feel great when you will achieve it", "stop when you are done"],
+  dreamIt: ["Build it", "Do it","Believe it","Create it","Do bigger","Go for it"],
+  yourLimitation: ["Comfort Zones", "it's your imagination", "stop before it's done","giveup","it's impossible"]
+}
